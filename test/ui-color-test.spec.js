15688b9f02d0e0aea9d51124d3ba3a4694d88f32
theoperatore <theoperatore@gmail.com>
2016-04-10 03:01:50 -0400

    updated to wait for backgrond color in tests

--- test/ui-color-test.spec.js
+++ test/ui-color-test.spec.js
@@ -6,7 +6,6 @@ import test from 'tape';
 import OSC from 'node-osc';
 import webdriver from 'selenium-webdriver';
 
-let until = webdriver.until;
 let By = webdriver.By;
 
 let driver = new webdriver.Builder()
@@ -15,12 +14,15 @@ let driver = new webdriver.Builder()
 
 let osc = new OSC.Client(process.env.OSC_SERVER_IP_ADDRESS, process.env.OSC_PORT);
 
-function findBackgroundColor() {
-  return driver.findElement(By.xpath("//div[@id='mount']/div")).then(content => {
-    return content.getAttribute('style').then(style => {
-      return /background-color\:\s+rgb\s*\(.+\)/.exec(style)[0];
+function waitForBackgroundColorToBe(color) {
+  return driver.wait(() => {
+    return driver.findElement(By.xpath("//div[@id='mount']/div")).then(content => {
+      return content.getAttribute('style').then(style => {
+        let testColor = /background-color\:\s+rgb\s*\(.+\)/.exec(style)[0];
+        return testColor === color;
+      })
     })
-  })
+  }, 10000);
 }
 
 test('Set background color correctly to 151 151 151', t => {
@@ -35,11 +37,9 @@ test('Set background color correctly to 151 151 151', t => {
       }
     })
 
-    findBackgroundColor().then(color => {
-      t.equals(color, 'background-color: rgb(151, 151, 151)', 'has background-color (151, 151, 151)');
-    }).catch(err => {
-      t.fail(err);
-    })
+    waitForBackgroundColorToBe('background-color: rgb(151, 151, 151)')
+      .then(() => t.pass('has background-color (151, 151, 151)'))
+      .catch(err => t.fail(err));
   })
 })
 
@@ -55,26 +55,24 @@ test('can change to a bunch of different colors', t => {
       }
     })
 
-    findBackgroundColor().then(color => {
-      t.equals(color, 'background-color: rgb(0, 0, 0)', 'has background-color (0, 0, 0)');
-    }).catch(err => {
-      t.fail(err);
-    }).then(() => {
-      osc.send('/color', ['121', '161', '21', '0'], err => {
-        if (err) {
-          t.fail(err);
-          osc.kill();
-          return;
-        }
-      })
+    return waitForBackgroundColorToBe('background-color: rgb(0, 0, 0)')
+      .then(() => t.pass('has background-color (0, 0, 0)'))
+      .catch(err => t.fail(err));
+      
+  }).then(() => {
 
-      findBackgroundColor().then(color => {
-        t.equals(color, 'background-color: rgb(121, 161, 21)', 'has background-color (121, 161, 21)');
-      }).catch(err => {
+    osc.send('/color', ['121', '161', '21', '0'], err => {
+      if (err) {
         t.fail(err);
-      })
-
+        osc.kill();
+        return;
+      }
     })
+
+    return waitForBackgroundColorToBe('background-color: rgb(121, 161, 21)')
+      .then(() => t.pass('has background-color (121, 161, 21)'))
+      .catch(err => t.fail(err));
+
   })
 })
 
