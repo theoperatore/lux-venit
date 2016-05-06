5091e8efbda68f49a7ca4aef9d0aaae070ea12a6
theoperatore <theoperatore@gmail.com>
2016-05-06 00:36:47 -0400

    added pulse and rumble feature

--- ui/App.js
+++ ui/App.js
@@ -12,25 +12,65 @@ export default React.createClass({
     }).isRequired,
   },
 
+  getInitialState() {
+    return {
+      pulse: false,
+      count: 0,
+      left: 0,
+      right: 0,
+      top: 0,
+      bottom: 0,
+    }
+  },
+
   renderWelcomeMessage() {
     return (
       <div>
-        <h4>Welcome!</h4>
-        <h4>You are conneced to <em>Lux Venit</em></h4>
+        <h4>Welcome to <em>Lux Venit</em></h4>
         <p>You are now part of an audience participation color projector!</p>
-        <p>For ultimate enjoyment:</p>
-        <ul>
-          <li>Turn up your screen brightness</li>
-          <li>Turn your screen sideways</li>
-          <li>Put the screen where you and everyone can see it</li>
-        </ul>
-        <br />
+        <p>Hold out your phone so that you can see both the piano and your phone.</p>
         <p>Enjoy...</p>
       </div>
     );
   },
 
+  startPulse(ev) {
+    let { width, height } = ev.target.getBoundingClientRect();
+    let y = ev.changedTouches ? ev.changedTouches[0].clientY : ev.clientY;
+
+    if (y <= (height / 2)) {
+      let count = 0;
+      let intervalId = setInterval(() => {
+        count += 0.25;
+
+        let left = Math.round(Math.random() * 20 - 10) + count;
+        let right = Math.round(Math.random() * 20 - 10) + count;
+        let top = Math.round(Math.random() * 20 - 10) + count;
+        let bottom = Math.round(Math.random() * 20 - 10) + count;
+
+        this.setState({ left, right, top, bottom });
+      }, 32);
+
+      this.setState({ intervalId });
+    } else {
+      this.setState({ pulse: true });
+    }
+  },
+
+  stopPulse() {
+    clearInterval(this.state.intervalId);
+    this.setState({ 
+      pulse: false,
+      left: 0,
+      right: 0,
+      top: 0,
+      bottom: 0,
+      intervalId: null,
+    });
+  },
+
   render() {
+    let { pulse, count, left, right, top, bottom } = this.state;
     let { rgba, fadeTime } = this.props.color;
     let [r, g, b] = rgba;
 
@@ -38,6 +78,19 @@ export default React.createClass({
       backgroundColor: `rgba(${r},${g},${b},1)`,
       transitionProperty: 'background-color',
       transitionDuration: `${fadeTime}ms`,
+      transitionTimingFunction: 'ease',
+      position: 'absolute',
+      top,
+      right,
+      left,
+      bottom,
+      overflowY: 'auto',
+    };
+
+    let pulseStyle = {
+      backgroundColor: pulse ? 'rgba(255, 255, 255, 0.45)' : 'transparent',
+      transitionProperty: 'background-color',
+      transitionDuration: pulse ? '0ms' : '300ms',
       transitionTimingFunction: 'ease',
       position: 'absolute',
       top: 0,
@@ -49,12 +102,20 @@ export default React.createClass({
 
     return (
       <div style={style}>
-        <div style={{ color: 'white' }}>
+        <div style={{ color: 'white', userSelect: 'none' }}>
           {
             fadeTime < 0
             ? this.renderWelcomeMessage()
             : ''
           }
+        </div>
+        <div 
+          style={pulseStyle}
+          onMouseDown={this.startPulse}
+          onMouseUp={this.stopPulse}
+          onTouchStart={this.startPulse}
+          onTouchEnd={this.stopPulse}
+        >
         </div>
       </div>
     )
